4a9c7ec3d5e5c041ca18d162cf51f03032c17aca
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-21 18:40:40 +0900

    test: add sops-nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -22,8 +22,8 @@ in {
   ];
 
   user' = {
-    # hashedPassword = "";
-    initialPassword = "123";
+    # hashedPasswordFile = "config.sops.secrets.pass-test.path;";
+    initialHashedPassword = "$y$j9T$FyXRRqLttcdVGvfopLg0g/$9gILD5hPZLqkwYi1nW9CFWp/VimrcZmssgtYv9qvVt1"
     isNormalUser = true;
     extraGroups = ["wheel"];
   };
--- modules/core/sops/default.nix
+++ modules/core/sops/default.nix
@@ -1,11 +1,19 @@
 {
+  config,
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  inherit (config.globals) homeDirectory configDirectory;
+in {
   imports = [inputs.sops-nix.nixosModules.sops];
   environment.systemPackages = with pkgs; [
     age
     sops
   ];
+  sops = {
+    defaultSopsFile = "${configDirectory}/secrets/secrets.yaml";
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
+    age.generateKey = true;
+  };
 }
