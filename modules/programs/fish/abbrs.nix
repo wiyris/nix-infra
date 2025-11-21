5c9ba34f3ed30441a79b1966bf62ea954c7668f2
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-21 10:09:50 +0900

    feat(fish): update abbrs

--- modules/programs/fish/abbrs.nix
+++ modules/programs/fish/abbrs.nix
@@ -3,7 +3,19 @@
     shellAbbrs = {
       k = "killall";
       kn = "killall -9";
-      sw = "";
+
+      nf = "nix flake";
+      nfc = "nix flake check";
+      nfu = "nix flake update";
+      npr = "nixpkgs-review pr --run fish --print-result";
+      nd = "nix develop --command fish";
+      nb = "nix build";
+      ns = "nix shell";
+      nr = "nix run";
+      ncg = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
+      nvd = "nvd --color always diff /run/current-system result | rg -v 0.0.0 | less -R";
+      sw = "nh os switch";
+      # sw = "nh os switch";
     };
   };
 }
