d4b7e14f892c12be94b3b2f83206e83d25e3a93d
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-21 10:17:50 +0900

    refactor(homesv): integrate hardware/netwroking settings to default file

--- hosts/homesv/default.nix
+++ hosts/homesv/default.nix
@@ -1,11 +1,60 @@
-{
+{pkgs, ...}: {
   imports = [
     ./disko.nix
     ./hardware-configuration.nix
-    ./hardware.nix
+    # ./hardware.nix
     ./initrd.nix
-    ./networking.nix
+    # ./networking.nix
   ];
+
+  programs' = {
+    bat.enable = true;
+    extra.enable = true;
+    eza.enable = true;
+    fish.enable = true;
+    fish.isDefault = true;
+    fzf.enable = true;
+    # git.enable = true;
+    nh.enable = true;
+    ripgrep.enable = true;
+    starship.enable = true;
+    tmux.enable = true;
+    yazi.enable = true;
+    zoxide.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    intel-gpu-tools
+    libva-utils
+  ];
+
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vpl-gpu-rt
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [intel-vaapi-driver];
+  };
+
+  networking = {
+    interfaces.enp2s0.ipv4.addresses = [
+      {
+        address = "192.168.0.42";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = "192.168.0.1";
+    nameservers = [
+      # "192.168.0.1"
+      "194.242.2.2"
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+  };
 
   boot = {
     loader = {
