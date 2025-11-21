7cb40ea7cec17a617617b812734fd17141300a9f
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-21 18:43:41 +0900

    fix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -7,23 +7,17 @@
   inherit (config.globals) userName;
 in {
   environment.systemPackages = with pkgs; [
-    neovim
-
     curl
-    # ffmpeg # record, convert music/video
     file # print filetype
-    # imagemagick # image editor
-    # jq # manipulate JSON
     killall
-    # lsof # print port process
+    neovim
     p7zip
     wget # download http/ftp
-    # yq # manipulate YAML
   ];
 
   user' = {
     # hashedPasswordFile = "config.sops.secrets.pass-test.path;";
-    initialHashedPassword = "$y$j9T$FyXRRqLttcdVGvfopLg0g/$9gILD5hPZLqkwYi1nW9CFWp/VimrcZmssgtYv9qvVt1"
+    initialHashedPassword = "$y$j9T$FyXRRqLttcdVGvfopLg0g/$9gILD5hPZLqkwYi1nW9CFWp/VimrcZmssgtYv9qvVt1";
     isNormalUser = true;
     extraGroups = ["wheel"];
   };
