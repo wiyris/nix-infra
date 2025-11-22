62a806c17c51c53b00e014529b76aaa732d5e963
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-22 12:48:59 +0900

    vm: update profile

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -7,13 +7,10 @@
   programs' = {
     bat.enable = true;
     btop.enable = true;
-    extra.enable = true;
     eza.enable = true;
     fish.enable = true;
-    fish.isDefault = true;
     fzf.enable = true;
     git.enable = true;
-    nh.enable = true;
     ripgrep.enable = true;
     starship.enable = true;
     tmux.enable = true;
