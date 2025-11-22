4da4af1867c49847e9b55b459ee44f668fe3a7b8
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-11-22 12:09:37 +0900

    test(tmux):

--- modules/programs/tmux/default.nix
+++ modules/programs/tmux/default.nix
@@ -10,36 +10,33 @@
 in {
   options.programs'.tmux.enable = lib.mkEnableOption {};
   config = lib.mkIf cfg.enable {
-    hm' = {
-      imports = [
-        ./dots/binds.nix
-        ./plugins/catppuccin.nix
+    imports = [
+      ./dots/binds.nix
+      ./plugins/catppuccin.nix
+    ];
+    programs.tmux = {
+      enable = true;
+      inherit shell;
+      mouse = true;
+      escapeTime = 0;
+      extraConfig = ''
+        set -g status-position top
+        # Start windows and panes at 1, not 0
+        set -g base-index 1
+        setw -g pane-base-index 1
+      '';
+      plugins = with pkgs.tmuxPlugins; [
+        # tmux-fzf
+        sensible
       ];
-      programs.tmux = {
-        enable = true;
-        inherit shell;
-        mouse = true;
-        escapeTime = 0;
-        extraConfig = ''
-          set -g status-position top
-          # Start windows and panes at 1, not 0
-          set -g base-index 1
-          setw -g pane-base-index 1
-        '';
-        plugins = with pkgs.tmuxPlugins; [
-          tmux-fzf
-          sensible
-        ];
-      };
+    };
 
-      programs.fzf.tmux.enableShellIntegration = true;
-      programs.fish.shellAbbrs = {
-        t = "tmux new-session";
-        ta = "tmux attach";
-        tl = "tmux list-sessions";
-        tk = "tmux kill-session -t";
-        tks = "tmux kill-server";
-      };
+    environment.shellAliases = {
+      t = "tmux new-session";
+      ta = "tmux attach";
+      tl = "tmux list-sessions";
+      tk = "tmux kill-session -t";
+      tks = "tmux kill-server";
     };
   };
 }
